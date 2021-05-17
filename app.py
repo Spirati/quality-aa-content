9f501163e7309753aa75261fc2d10b5e9f548cf4
Lynne Cure <lynne@lynnux.org>
2021-05-17 18:05:29 -0400

    change to app factory

--- app.py
+++ app.py
@@ -8,30 +8,31 @@ def load_config() -> dict:
     with open("conf/config.json") as config:
         return json.load(config)
 
-config = load_config()
+def create_app():
+    config = load_config()
 
-characters = char.generate_character_list(
-    config.get("use_stored_characters", False)
-)
+    characters = char.generate_character_list(
+        config.get("use_stored_characters", False)
+    )
 
-print(characters["Phoenix Wright"])
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route("/")
+    def home_page():
+        return """
+        <h1>Welcome to the Randomly Generated Ace Attorney Shitpost Hellhole!</h1>
+        <h2><i>Home to only the highest quality of wildly overdone memes!</i></h2>
 
-@app.route("/")
-def home_page():
-    return """
-    <h1>Welcome to the Randomly Generated Ace Attorney Shitpost Hellhole!</h1>
-    <h2><i>Home to only the highest quality of wildly overdone memes!</i></h2>
+        <p>Try it out!</p>
+        <ul>
+            <li><a href="/quotes?num=5">"Incorrect" quotes</a></li>
+            <li><a href="/mulaney?num=5">AA characters as John Mulaney quotes</a></li>
+        </ul>
+        """
 
-    <p>Try it out!</p>
-    <ul>
-        <li><a href="/quotes?num=5">"Incorrect" quotes</a></li>
-        <li><a href="/mulaney?num=5">AA characters as John Mulaney quotes</a></li>
-    </ul>
-    """
+    @app.route("/<category>")
+    def quote_template(category: str):
+        num_quotes = int(request.args.get('num', 1))
+        return "<hr/>".join([templates.random_template(category, characters) for _ in range(num_quotes)])
 
-@app.route("/<category>")
-def quote_template(category: str):
-    num_quotes = int(request.args.get('num', 1))
-    return "<hr/>".join([templates.random_template(category, characters) for _ in range(num_quotes)])
+    return app
