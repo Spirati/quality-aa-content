edeb1b338b1fa0be6c3fa64c40dbdc6d404d5f81
Lynne Cure <lynne@lynnux.org>
2021-05-17 19:06:42 -0400

    Add footer, home link

--- app.py
+++ app.py
@@ -28,11 +28,15 @@ def create_app():
             <li><a href="/quotes?num=5">"Incorrect" quotes</a></li>
             <li><a href="/mulaney?num=5">AA characters as John Mulaney quotes</a></li>
         </ul>
+        <hr/>
+        <footer>
+            <p><i>Written and maintained by <a href="https://lynnux.org">Lynnux</a>. View the <a href="https://github.com/Spirati/quality-aa-content">source code</a> or talk to me on <a href="https://twitter.com/_lynnux">Twitter</a>!<i></p>
+        </footer>
         """
 
     @app.route("/<category>")
     def quote_template(category: str):
         num_quotes = int(request.args.get('num', 1))
-        return "<hr/>".join([templates.random_template(category, characters) for _ in range(num_quotes)])
+        return "<hr/>".join(["<a href=\"/\">Go home</a>"] + [templates.random_template(category, characters) for _ in range(num_quotes)])
 
     return app
